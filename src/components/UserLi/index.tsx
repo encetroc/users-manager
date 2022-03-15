2165d2213a512d23210a0f23da38d035cb72ae5f
elmehdi rhindi <g6handi@gmail.com>
2022-03-15 18:28:35 +0100

    useform

--- src/components/UserLi/index.tsx
+++ src/components/UserLi/index.tsx
@@ -1,23 +1,25 @@
 import './UserLi.css'
 import { User } from 'types'
+import { useNavigate } from 'react-router-dom'
 
 type UserLiProps = {
   user: User
 }
 
 export function UserLi({ user }: UserLiProps) {
+  const navigate = useNavigate()
   return (
-    <li className='user_li'>
+    <li className="user_li">
       <div className="user_info">
-        <img
-          width={48}
-          height={48}
-          src={user.picture.thumbnail}
-          alt="avatar"
-        />
+        <img width={48} height={48} src={user.picture.thumbnail} alt="avatar" />
         <p>{`${user.name.title}. ${user.name.first} ${user.name.last}`}</p>
       </div>
-      <button className="view_more">View more</button>
+      <button
+        onClick={() => navigate(`/user/${user.login.username}`)}
+        className="view_more"
+      >
+        View more
+      </button>
     </li>
   )
 }
--- src/routes/UserDetails/index.tsx
+++ src/routes/UserDetails/index.tsx
@@ -1,3 +1,66 @@
+import { useUsers } from 'context'
+import { useMemo } from 'react'
+import { useForm } from 'react-hook-form'
+import { useParams } from 'react-router-dom'
+import { User } from 'types'
+import './UserDetails.css'
+
 export function UserDetails() {
-  return <div>user details</div>
+  const { state } = useUsers()
+  const { id } = useParams()
+
+  const user: User | null = useMemo(() => {
+    if (id) {
+      return state[id]
+    } else {
+      return null
+    }
+  }, [id, state])
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      ...user?.name,
+    },
+  })
+  return (
+    <>
+      {user && (
+        <div className="user_details_container">
+          <img width={128} height={128} src={user.picture.large} alt="avatar" />
+          <section>
+            <h2>Name</h2>
+            <form style={{ display: 'flex', flexDirection: 'column' }}>
+              <label htmlFor="title">Title</label>
+              <input
+                {...register('title', { required: true })}
+                style={{ backgroundColor: 'black' }}
+                type="text"
+                id="title"
+              />
+              <label htmlFor="first">First name</label>
+              <input
+                {...register('first', { required: true })}
+                style={{ backgroundColor: 'black' }}
+                type="text"
+                id="first"
+              />
+              <label htmlFor="last">Last Name</label>
+              <input
+                {...register('last', { required: true })}
+                style={{ backgroundColor: 'black' }}
+                type="text"
+                id="last"
+              />
+            </form>
+          </section>
+        </div>
+      )}
+    </>
+  )
 }
